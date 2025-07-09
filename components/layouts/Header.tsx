222972cb4d17d3619ce2db58aa6c9139af50ed18
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-09 20:22:32 +0600

    header part is completed

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,24 +1,43 @@
-import React from 'react'
-import { Carousel, CarouselContent, CarouselItem } from '../ui/carousel'
-import Image from 'next/image'
-import { Button } from '../ui/button'
+import React from "react";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
+import Image from "next/image";
+import { Button } from "../ui/button";
 
 export default function Header() {
   return (
     <header>
-        <Carousel>
-            <CarouselContent>
-                <CarouselItem className=' relative'> 
-                <Image src={'/images/header-banner.png'} width={1000} height={1000} alt='header banner image' className='w-full h-[90vh]'/>
-                <div className='bg-(--bgPrimary) w-[643px] p-6 fixed top-[40%] right-[5%]'>
-<p>New Arrival</p>
-<h1 className='text-5xl font-bold text-(--textPrimary)'>Discover Our New Collection</h1>
-<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut elit tellus, luctus nec ullamcorper mattis.</p>
-<Button variant={'default'} size={'lg'} className='border-0 px-10 py-6'>BUY NOW</Button>
-                </div>
-                </CarouselItem>
-            </CarouselContent>
-        </Carousel>
+      <Carousel>
+        <CarouselContent>
+          {Array.from({ length: 3 }).map((item, i) => (
+            <CarouselItem key={i} className=" relative">
+              <Image
+                src={"/images/header-banner.png"}
+                width={1000}
+                height={1000}
+                alt="header banner image"
+                className="w-full h-[90vh]"
+              />
+              <div className="bg-(--bgPrimary) w-[643px] p-8 fixed top-[30%] right-[5%] rounded-lg space-y-2">
+                <p className="text-base font-semibold">New Arrival</p>
+                <h1 className="text-6xl font-bold leading-tight text-(--textPrimary)">
+                  Discover Our New Collection
+                </h1>
+                <p className="text-lg font-medium">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
+                  elit tellus, luctus nec ullamcorper mattis.
+                </p>
+                <Button
+                  variant={"default"}
+                  size={"lg"}
+                  className="rounded-none bg-(--textPrimary) px-20 py-8 mt-5"
+                >
+                  BUY NOW
+                </Button>
+              </div>
+            </CarouselItem>
+          ))}
+        </CarouselContent>
+      </Carousel>
     </header>
-  )
+  );
 }
---
+++ components/ui/Title.tsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export default function Title() {
+  return (
+    <h2 className='text-4xl font-bold'>Browse The Range</h2>
+  )
+}
