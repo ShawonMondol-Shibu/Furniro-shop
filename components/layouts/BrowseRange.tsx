0bc34202d04bfc3c1ca7185f1f416feeae89f9e7
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-09 23:21:26 +0600

    browseRange is updated

--- components/layouts/BrowseRange.tsx
+++ components/layouts/BrowseRange.tsx
@@ -1,17 +1,42 @@
-import React from 'react'
-import Title from '../ui/Title'
-import Image from 'next/image'
+import React from "react";
+import Title from "../ui/Title";
+import Image from "next/image";
 
+interface dataType {
+  title: string;
+  image: string;
+}
 export default function BrowseRange() {
+  const rangeData = [
+    { title: "Dining", image: "/images/image.png" },
+    { title: "Living", image: "/images/image1.png" },
+    { title: "Bedroom", image: "/images/image2.png" },
+  ];
   return (
-    <main>
-      <Title> Browse The Range</Title>
-      <div>
-<div className='rounded-lg'>
-  <Image src={'/images/image.png'} width={381} height={480} alt='range image' className='rounded-lg'/>
-  <h3 className='text-2xl font-semibold'>Dining</h3>
-</div>
+    <main className="py-20">
+      <div className="text-center mb-10">
+        <Title> Browse The Range</Title>
+        <p className="text-xl font-normal">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+        </p>
+      </div>
+
+      <div className="flex items-center justify-center gap-10">
+        {rangeData.map((data: dataType, i) => (
+          <div key={i} className="rounded-lg ">
+            <Image
+              src={data.image}
+              width={381}
+              height={480}
+              alt="range image"
+              className="rounded-lg"
+            />
+            <h3 className="text-2xl font-semibold text-center mt-8">
+              {data.title}
+            </h3>
+          </div>
+        ))}
       </div>
     </main>
-  )
+  );
 }
