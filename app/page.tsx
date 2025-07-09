d0039eddcd0a1a19902244a9a6eb0cd2ca13f84d
shawonmondol-shibu <shawonmondolshibu@gmail.com>
2025-07-09 20:25:06 +0600

    header part is added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,11 @@
+import BrowseRange from "@/components/layouts/BrowseRange";
 import Header from "@/components/layouts/Header";
 
 export default function Home() {
   return (
     <>
       <Header/>
+      <BrowseRange/>
     </>
   );
 }
