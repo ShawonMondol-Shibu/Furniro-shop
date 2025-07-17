9374470187ebd0c4c605ebc9a9723158b7991ef3
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-18 02:53:50 +0600

    popover added

--- app/shop/page.tsx
+++ app/shop/page.tsx
@@ -17,7 +17,13 @@ import {
 } from "@/components/ui/pagination";
 import Title from "@/components/ui/Title";
 import ProductCard from "@/components/layouts/ProductCard";
-import { BadgeCheck, Headset, SlidersHorizontal, Trophy, Truck } from "lucide-react";
+import {
+  BadgeCheck,
+  Headset,
+  SlidersHorizontal,
+  Trophy,
+  Truck,
+} from "lucide-react";
 import { HiViewGrid } from "react-icons/hi";
 import { BsViewList } from "react-icons/bs";
 import { Button } from "@/components/ui/button";
@@ -56,22 +62,39 @@ export default function Page() {
         </div>
       </header>
 
-      <section className="bg-(--bgPrimary) p-5">
+      {/* Filter is added here */}
+      <section className="bg-(--bgPrimary) p-5 flex items-center justify-between">
+        {/* Filter left side */}
         <div className="h-6 flex items-center gap-2 ">
-          <Button variant={'ghost'} size={'lg'} className="text-xl font-normal">
-            <SlidersHorizontal size={25}/>
+          <Button variant={"ghost"} size={"lg"} className="text-xl font-normal">
+            <SlidersHorizontal size={25} />
             Filter
           </Button>
-          <Button variant={'ghost'} size={'icon'}>
-            <HiViewGrid size={25}/>
+          <Button variant={"ghost"} size={"icon"}>
+            <HiViewGrid size={25} />
           </Button>
-          <Button variant={'ghost'} size={'icon'}>
-            <BsViewList size={25}/>
+          <Button variant={"ghost"} size={"icon"}>
+            <BsViewList size={25} />
           </Button>
-          <Separator orientation="vertical" className="border-(--textPrimary) border"/>
+          <Separator
+            orientation="vertical"
+            className="border-(--textPrimary) border"
+          />
           <p>Showing 1–16 of 32 results</p>
         </div>
-      
+
+{/* Filter right side */}
+        <div className="flex items-center gap-5">
+<div className="flex items-center-safe gap-5">
+  <p>Show</p>
+  <span className="px-4 py-3 bg-white text-(--textGray)">16</span>
+</div>
+
+<div className="flex items-center-safe gap-5">
+  <p>Short by</p>
+  
+</div>
+        </div>
       </section>
 
       <main>
