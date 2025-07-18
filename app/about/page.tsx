94110204dda4a2f179404e9267aff2b7814278a7
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-19 02:30:36 +0600

    categories added on about page

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -4,8 +4,19 @@ import BlogCard from "./BlogCard";
 import Services from "@/components/layouts/Services";
 import SitePagination from "@/components/layouts/SitePagination";
 import { Input } from "@/components/ui/input";
-
+import Link from "next/link";
+interface categoryType {
+  titles: string;
+  items: number;
+}
 export default function Page() {
+  const categorys = [
+    { titles: "Crafts", items: 2 },
+    { titles: "Design", items: 8 },
+    { titles: "Handmade", items: 7 },
+    { titles: "Interior", items: 1 },
+    { titles: "Wood", items: 6 },
+  ];
   return (
     <div>
       <HeaderBreadCrumb
@@ -22,18 +33,30 @@ export default function Page() {
           </section>
 
           <aside className="flex-1">
-<Input/>
-<div>
-  Categories
-</div>
-<div>
-  Recent Posts
-</div>
+            <div>
+              <Input />
+              <ul className="px-5">
+                <h3>Categories</h3>
+                {categorys.map((category: categoryType, i: number) => (
+                  <li key={i}>
+                    <Link
+                      href={""}
+                      className="flex items-center justify-between"
+                    >
+                      <span>{category.titles}</span>
+
+                      <span>{category.items}</span>
+                    </Link>
+                  </li>
+                ))}
+              </ul>
+            </div>
+
+            
+            <div>Recent Posts</div>
           </aside>
         </div>
-
         <SitePagination />
-
       </main>
       <Services />
     </div>
