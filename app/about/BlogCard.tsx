33a8229b4bf6485996d5594bc3c932bd33fbb3a2
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-18 08:17:29 +0600

    blog & blog card is updated

--- app/about/BlogCard.tsx
+++ app/about/BlogCard.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { CardDescription, CardTitle } from "@/components/ui/card";
 import { UserRound } from "lucide-react";
 import Image from "next/image";
@@ -7,17 +8,30 @@ import React from "react";
 export default function BlogCard() {
   return (
     <div>
-      <Image src={"/blogs/image.png"} width={500} height={500} alt="" />
+      <Image src={"/blogs/image.png"} width={817} height={500} alt="" />
       <div>
-        <span>
-          <UserRound />
+        <span className="flex items-center text-(--textGray)">
+          <UserRound fill="gray" />
           Admin
         </span>
       </div>
 
-      <CardTitle>Going all-in with millennial design</CardTitle>
-      <CardDescription> Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae ultricies leo integer malesuada nunc. In nulla posuere sollicitudin aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero. Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et ultrices neque ornare aenean euismod elementum.</CardDescription>
-      <Link href={'#'}>Read More</Link>
+      <CardTitle className="text-3xl font-medium">Going all-in with millennial design</CardTitle>
+      <CardDescription className="text-base font-normal">
+        {" "}
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+        tempor incididunt ut labore et dolore magna aliqua. Mus mauris vitae
+        ultricies leo integer malesuada nunc. In nulla posuere sollicitudin
+        aliquam ultrices. Morbi blandit cursus risus at ultrices mi tempus
+        imperdiet. Libero enim sed faucibus turpis in. Cursus mattis molestie a
+        iaculis at erat. Nibh cras pulvinar mattis nunc sed blandit libero.
+        Pellentesque elit ullamcorper dignissim cras tincidunt. Pharetra et
+        ultrices neque ornare aenean euismod elementum.
+      </CardDescription>
+      <Button variant={'link'} size={'lg'} className="text-base font-normal" asChild>
+
+      <Link href={"#"}>Read More</Link>
+      </Button>
     </div>
   );
 }
--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,10 +1,22 @@
-import HeaderBreadCrumb from '@/components/layouts/HeaderBreadCrumb'
-import React from 'react'
+import HeaderBreadCrumb from "@/components/layouts/HeaderBreadCrumb";
+import React from "react";
+import BlogCard from "./BlogCard";
 
 export default function Page() {
   return (
     <div>
-      <HeaderBreadCrumb title='Blog' breadCrumbPage='Home' breadCrumbLink='Blog'/>
+      <HeaderBreadCrumb
+        title="Blog"
+        breadCrumbPage="Home"
+        breadCrumbLink="Blog"
+      />
+      <main className="container m-auto mt-20 grid grid-cols-2">
+        <section>
+          <BlogCard />
+        </section>
+
+        <aside></aside>
+      </main>
     </div>
-  )
+  );
 }
