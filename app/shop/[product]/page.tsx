5539d9e0ced3119e51bd98a9fcc2a806f3d2862c
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-20 05:33:28 +0600

    Product Details updated

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -6,6 +6,8 @@ import {
   BreadcrumbPage,
   BreadcrumbSeparator,
 } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { Star } from "lucide-react";
 import React from "react";
 
 export default function Page() {
@@ -31,9 +33,25 @@ export default function Page() {
         {/* Product Details Section */}
         <section>
           <div></div>
-          
+
           <div>
             <h3>Asgaard Sofa</h3>
+            <h5>Rs 250,000.00</h5>
+            <span>
+              <Star /> | 5 Customer Review
+            </span>
+            <p>
+              Setting the bar as one of the loudest speakers in its class, the
+              Kilburn is a compact, stout-hearted hero with a well-balanced
+              audio which boasts a clear midrange and extended highs for a
+              sound.
+            </p>
+            <div>
+              Size
+              <Button>L</Button>
+              <Button>XL</Button>
+              <Button>XS</Button>
+            </div>
           </div>
         </section>
       </main>
