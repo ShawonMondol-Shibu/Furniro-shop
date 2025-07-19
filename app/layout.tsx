c12afb792aff6203eeb5250dbbecf4c22a967709
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-20 00:57:26 +0600

    contact is updated

--- app/layout.tsx
+++ app/layout.tsx
@@ -23,7 +23,7 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body
-        className={`${poppins.variable} antialiased`}
+        className={`${poppins.className} antialiased`}
       >
         <Navbar/>
 
