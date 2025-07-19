717d3b20369d45e298be724c76259cd17558a40b
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-20 05:25:49 +0600

    Product Details Section added

--- app/shop/[product]/page.tsx
+++ app/shop/[product]/page.tsx
@@ -11,17 +11,32 @@ import React from "react";
 export default function Page() {
   return (
     <div>
-      <Breadcrumb>
-        <BreadcrumbList>
-          <BreadcrumbItem>
-            <BreadcrumbPage>Home</BreadcrumbPage>
-          </BreadcrumbItem>
-          <BreadcrumbSeparator />
-          <BreadcrumbItem>
-            <BreadcrumbLink href="/shop">Shop</BreadcrumbLink>
-          </BreadcrumbItem>
-        </BreadcrumbList>
-      </Breadcrumb>
+      <header className="bg-(--bgPrimary) py-8">
+        <div className="container m-auto">
+          <Breadcrumb>
+            <BreadcrumbList>
+              <BreadcrumbItem>
+                <BreadcrumbPage>Home</BreadcrumbPage>
+              </BreadcrumbItem>
+              <BreadcrumbSeparator />
+              <BreadcrumbItem>
+                <BreadcrumbLink href="/shop">Shop</BreadcrumbLink>
+              </BreadcrumbItem>
+            </BreadcrumbList>
+          </Breadcrumb>
+        </div>
+      </header>
+
+      <main>
+        {/* Product Details Section */}
+        <section>
+          <div></div>
+          
+          <div>
+            <h3>Asgaard Sofa</h3>
+          </div>
+        </section>
+      </main>
     </div>
   );
 }
