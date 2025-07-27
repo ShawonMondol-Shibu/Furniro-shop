670ebae1d04be37bd03c2e72e21fb11231a2e67e
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-07-27 16:00:21 +0600

    services updated

--- components/layouts/Services.tsx
+++ components/layouts/Services.tsx
@@ -13,13 +13,13 @@ export default function Services() {
     { icon: Headset, title: "24 / 7 Support", desc: "Dedicated support" },
   ];
   return (
-    <section className="bg-(--bgPrimary) px-10 py-32 grid gap-10 grid-cols-4 items-center justify-center">
+    <section className="bg-(--bgPrimary) px-5 py-32 grid gap-10 lg:grid-cols-4 md:grid-cols-2 items-center justify-center ">
       {services.map((service: serviceType, i: number) => (
         <div key={i} className="flex items-center gap-2 m-auto">
           <service.icon size={60} />
           <div>
-            <h3 className="text-2xl font-semibold">{service.title}</h3>
-            <p className="text-xl font-medium text-(--textGray)">
+            <h3 className="xl:text-2xl md:text-xl font-semibold">{service.title}</h3>
+            <p className="xl:text-xl md:text-lg font-medium text-(--textGray)">
               {service.desc}
             </p>
           </div>
