ef3ad00311a176f76783f3cd2e7efd9a05cf8abb
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-01 00:20:21 +0600

    product details updated

--- components/layouts/OurProducts.tsx
+++ components/layouts/OurProducts.tsx
@@ -1,4 +1,4 @@
-"use clients"
+"use client";
 import React, { useEffect, useState } from "react";
 import Title from "../ui/Title";
 import { Button } from "../ui/button";
@@ -7,27 +7,33 @@ import ProductCard from "./ProductCard";
 import { dataType } from "@/app/shop/page";
 
 export default function OurProducts() {
-  const [products, setProducts] = useState<dataType[]>();
-  useEffect(()=>{
-fetch("/furnitureProducts.josn").then((res)=>res.json()).then(data=>setProducts(data))
-  },[])
+   const [products, setProduct] = useState<dataType[]>();
+    useEffect(() => {
+      fetch("/furnitureProducts.json")
+        .then((res) => res.json())
+        .then((data) => setProduct(data));
+    }, []);
+    if(!products){
+return <h3 className="text-center">Loading....</h3>;
+    }
   return (
     <main className="container m-auto py-5">
       <div className="text-center">
         <Title className={"text-[40px] text-center"}> Our Products </Title>
       </div>
       <div className="grid lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 xl:gap-20 gap-10 items-center justify-center py-10 px-5">
-        {products.map((item:dataType) => (
-          <ProductCard
-            key={item.id}
-            image={item.}
-            title={"Asgaard Sofa"}
-            category={"Luxury Big Sofa"}
-            price={100}
-            savings={0}
-            url="/shop/product"
-          />
-        ))}
+        {products.map((item: dataType) => (
+                  <ProductCard
+                    key={item.id}
+                    image={item.image}
+                    title={item.productName}
+                    category={item.shortDescription.slice(0, 10)}
+                    currency={item.currency}
+                    price={item.price}
+                    savings={1200}
+                    url={`/shop/${item.id}`}
+                  />
+                ))}
       </div>
       <div className="w-full text-center">
         <Button
