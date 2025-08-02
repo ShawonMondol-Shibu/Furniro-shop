e1351653fc8f35cf8e0f9c99372bcd4f01f43516
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-02 15:09:41 +0600

    shop product is updated

--- components/layouts/ProductCard.tsx
+++ components/layouts/ProductCard.tsx
@@ -10,7 +10,7 @@ export type productCardType = {
   price: number;
   savings: number;
   url: string;
-  currency:string;
+  currency: string;
 };
 
 export default function ProductCard({
@@ -26,15 +26,19 @@ export default function ProductCard({
     <>
       <Link href={url} className="m-auto">
         <Card className="xl:w-72 sm:w-56  pt-0 bg-(--cardBg) hover:scale-105 border-none rounded-none shadow-none hover:shadow-xl m-auto group transition-all duration-200 ease-linear">
-          <Image
-            src={image}
-            width={285}
-            height={301}
-            alt={category}
-            className="m-auto h-[285px] w-[285px] object-contain"
-          />
+          <div className="m-auto h-[285px] w-[285px] flex items-center">
+            <Image
+              src={image}
+              width={285}
+              height={301}
+              alt={category}
+              className="object-contain m-auto"
+            />
+          </div>
           <CardFooter className="block space-y-2">
-            <CardTitle className="text-2xl font-semibold group-hover:text-(--textPrimary) transition-colors duration-200 ease-in-out">{title}</CardTitle>
+            <CardTitle className="text-2xl font-semibold group-hover:text-(--textPrimary) transition-colors duration-200 ease-in-out">
+              {title}
+            </CardTitle>
             <p className="text-base font-medium">{category}...</p>
             <h4 className="text-xl font-semibold flex items-center justify-between">
               {currency} {price}
