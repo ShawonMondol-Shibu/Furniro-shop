afc87f326e66c79cc289af2fb9519f605dfa5fdd
ShawonMondol-Shibu <shawonmondolshibu@gmail.com>
2025-08-25 20:32:32 +0600

    header is updated

--- components/layouts/Header.tsx
+++ components/layouts/Header.tsx
@@ -1,9 +1,5 @@
 import React from "react";
-import {
-  Carousel,
-  CarouselContent,
-  CarouselItem,
-} from "../ui/carousel";
+import { Carousel, CarouselContent, CarouselItem } from "../ui/carousel";
 import { Button } from "../ui/button";
 
 export default function Header() {
@@ -20,21 +16,22 @@ export default function Header() {
           {Array.from({ length: 3 }).map((_, i) => (
             <CarouselItem
               key={i}
-              className="relative w-full h-[90dvh] bg-[url('/images/header-banner.png')] bg-no-repeat bg-top bg-contain flex items-center justify-between"
+              className=" w-full h-[95dvh] bg-[url('/images/header-banner.png')] bg-no-repeat bg-top bg-contain"
             >
-              <div className="absolute left-1/2 -translate-x-1/2 top-1/2 -translate-y-1/2 p-8 rounded-lg space-y-4 bg-[var(--bgPrimary)] max-w-xl text-center">
-                <p className="text-base font-semibold text-white">New Arrival</p>
-                <h1 className="text-5xl md:text-6xl font-bold leading-tight text-[var(--textPrimary)]">
+              <div className="xl:p-8 lg:p-6 md:p-4 p-2 rounded-lg md:space-y-2 space-y-0 bg-[var(--bgPrimary)] xl:max-w-lg lg:max-w-96 md:max-w-80 max-w-70 text-center">
+                <p className="xl:text-base lg:text-sm text-xs font-semibold">
+                  New Arrival
+                </p>
+                <h1 className="xl:text-5xl lg:text-3xl text-xl font-bold leading-tight text-[var(--textPrimary)]">
                   Discover Our New Collection
                 </h1>
-                <p className="text-lg font-medium text-white">
+                <p className="xl:text-lg lg:text-base text-sm font-medium">
                   Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut
                   elit tellus, luctus nec ullamcorper mattis.
                 </p>
                 <Button
                   variant="default"
-                  size="lg"
-                  className="rounded-none px-10 py-6 mt-4 bg-[var(--textPrimary)] text-white"
+                  className="rounded-none mt-4 bg-[var(--textPrimary)]  text-white"
                 >
                   BUY NOW
                 </Button>
